d87d1f7e776b0cc9a94d454cac36a21b76137a9a
LauraSanchez24 <lauracamilasanchezvillegas@gmail.com>
2023-12-11 21:33:33 -0500

    nueva actualizacion

--- src/app/Shared/pqrs-form/pqrs-form.component.ts
+++ src/app/Shared/pqrs-form/pqrs-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/Core/auth.service';
 import { LocalesService } from 'src/app/Core/locales.service';
 import { PqrsService } from 'src/app/Core/pqrs.service';
 
@@ -9,11 +12,25 @@ import { PqrsService } from 'src/app/Core/pqrs.service';
 })
 export class PqrsFormComponent implements OnInit{
   listLocales:any[]=[]
+  formPqr:FormGroup
+  usuario:any
+  id=0
   constructor(private service:PqrsService,
-              private serviceLocales:LocalesService
-      ){}
+              private serviceLocales:LocalesService,
+              private auth:AuthService,
+              private toars:ToastrService
+      ){
+        this.formPqr=new FormGroup({
+          local:new FormControl(null,[Validators.required]),
+          descripcion:new FormControl(null,[Validators.required])
+        })
+      }
 
   ngOnInit(): void {
+    let usu=localStorage.getItem('usuario')
+    if (usu) {
+      this.usuario=JSON.parse(usu)
+    }
     this.getList();
   }
 
@@ -22,4 +39,17 @@ export class PqrsFormComponent implements OnInit{
       this.listLocales=result;
     })
   }
+  enviarPqr(){
+    let  data={  
+       "num_radicado": 3636,
+       "peticion_pqr_id": 1,
+       "descripcion": this.formPqr.controls['descripcion'].value,
+       "local_id": this.formPqr.controls['local'].value,
+       "usuario_id": this.usuario.id
+   } 
+   console.log(data)
+  //  this.service.save(data, this.id).subscribe(result=>{
+  //    this.toars.success(this.usuario.username+' tu peticion fue enviada correctamente', 'ShoesCompay')
+  //  })
+   }
 }
